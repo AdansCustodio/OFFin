4578ec96db1b4dbdee8b8b6d3f196f643f3ad74c
AdansCustodio <adansfenix@gmail.com>
2026-04-16 10:09:10 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        // Configuramos o Vite para processar os dois pontos de entrada
+        main: resolve(__dirname, 'index.html'),
+        app: resolve(__dirname, 'app.html'),
+      },
+    },
+  },
 })

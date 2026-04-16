9be3b3ac2cac1bb0a724ebd9b26e832a78f0ac4f
AdansCustodio <adansfenix@gmail.com>
2026-04-16 09:57:04 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
+/**
+ * Este é o ficheiro de entrada do seu aplicativo.
+ * Ele seleciona o elemento 'root' no seu app.html e renderiza
+ * o componente principal <App /> dentro dele.
+ */
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
